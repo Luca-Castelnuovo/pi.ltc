e680d844401f38a9265c490a6735f93b463c819e
Luca Castelnuovo <luca@castelnuovo.xyz>
2022-08-15 01:05:48 +0200

    Version 1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo json_encode(["hello" => "world"]);
+require '../vendor/autoload.php';
+
+use PiPHP\GPIO\GPIO;
+use PiPHP\GPIO\Pin\PinInterface;
+
+// TODO: paseto tokens
+
+// TODO: email
+
+// TODO: validate authorization header
+// maybe with JWT?
+
+$gpio = new GPIO();
+
+// Retrieve pin 18 and configure it as an output pin
+$pin = $gpio->getOutputPin(18);
+
+// Turn it on
+$pin->setValue(PinInterface::VALUE_HIGH);
+
+sleep(5);
+
+// Turn it off
+$pin->setValue(PinInterface::VALUE_LOW);
+
+echo 'done';
