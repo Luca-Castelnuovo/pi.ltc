007877ce5ff42f9023177ee519a98adf5d647619
Luca Castelnuovo <luca@castelnuovo.xyz>
2022-08-16 00:33:53 +0200

    Move to JWT instead of paseto for php7

--- includes/auth.php
+++ includes/auth.php
@@ -1,17 +1,12 @@
 <?php
 
-use CQ\Crypto\Helpers\Token;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 function check_auth(?string $token)
 {
-    $data = Token::decrypt(
-        key: $_ENV['TOKEN_KEY'],
-        token: $token
+    return JWT::decode(
+        $token,
+        new Key($_ENV['JWT_KEY'], 'HS256')
     );
-
-    if ($data->expiration < time()) {
-        throw new Exception('Token expired');
-    }
-
-    return $data;
 }
